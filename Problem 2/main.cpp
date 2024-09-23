a3350b0da3069942ad4dedb1ae80d608af3b3da8
panjieehh24 <osapanji8@gmail.com>
2024-09-23 21:21:49 +0700

    Update main.cpp

--- Problem 2/main.cpp
+++ Problem 2/main.cpp
@@ -9,13 +9,10 @@ int main() {
 	int n;
 	cin>>n;
 
-	int sum=0;
-
-	for(int i=1; i<=n; i++)
+	for(int i=1; i<=10; i++)
 	{
-		sum+=i;
+		int cnt = n*i;
+		cout<<n<<" x "<<i<<" = "<<cnt<<'\n';
 	}
-
-	cout<<"Sum = "<<sum<<'\n';
 	
 }
